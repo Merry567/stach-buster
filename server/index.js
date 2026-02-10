9b39ad039a94e8ba0b191769e17e20d1478a0cd4
Merry567 <meredithwonson@gmail.com>
2026-02-10 12:32:23 -0500

    mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Stash Buster API running");
