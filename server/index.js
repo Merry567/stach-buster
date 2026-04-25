4fb0f63852c0833733d54f053edc956ab6255228
Merry567 <meredithwonson@gmail.com>
2026-04-25 10:15:12 -0400

    vercel fix 0.3

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const { initGridFS } = require("./config/gridfs");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://stash-buster.vercel.app/",
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
